efa544964a36851df036571f55baa07e55167b28
ckagiri <charleskagiri@hotmail.com>
2016-11-27 01:38:08 +0300

    made league -model implement a pojo

--- src/server/api/league/league.model.ts
+++ src/server/api/league/league.model.ts
@@ -1,8 +1,15 @@
 import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+export interface ILeague {
+    name: string;
+};
+
+interface ILeagueModel extends ILeague, mongoose.Document { }
+
+
 const leagueSchema = new Schema({
   name: {type: String, required: true, trim: true}
 });
 
-export const League = mongoose.model('League', leagueSchema);
+export const League = mongoose.model<ILeagueModel>('League', leagueSchema);
