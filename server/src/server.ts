90ee1b1a9133fb4d63fc87a956a75534314b3b69
ckagiri <charleskagiri@hotmail.com>
2016-12-12 03:36:39 +0300

    added aurelia ts skeleton as a client-side starter

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
-import * as http from "http";
-import {routes} from './routes'
+import * as http from 'http';
+import * as path from 'path';
+import {routes} from './routes';
+let favicon = require('serve-favicon');
 
 mongoose.connect("mongodb://localhost/ligi");
 const app = express();
@@ -11,6 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
+
+app.use(favicon(path.join(__dirname, '../../client') + '/favicon.ico'));
+app.use(express.static(path.join(__dirname, '../../client')));
 
 const port = 8007;
 const server: http.Server = app.listen(port, function() {
