b5a0f9095fcf9a8fef611fd5467fb2928b036d46
Charles Kagiri <charleskagiri@hotmail.com>
2017-05-17 13:05:02 +0300

    thoughts

--- src/server/app/apiRoutes.ts
+++ src/server/app/apiRoutes.ts
@@ -2,13 +2,8 @@ import {Router} from 'express';
 import {profileRouter} from './api/user/profile';
 import {leagueRouter} from './api/league/league.route';
 import { send404, notFoundMiddleware } from './utils/notfound'; 
-import data = require('./data');
 
 const router = Router();
-
-router.get('/people', (req, res, next) => {
-	res.status(200).send(data.getPeople());
-});
 
 router.get('/ping', (req, res) => {
   res.json({pong: Date.now()})
