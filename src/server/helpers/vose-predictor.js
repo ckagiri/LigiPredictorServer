bdd641ee49fe5bb284eb01772de0c65b6db10569
ckagiri <charleskagiri@gmail.com>
2017-06-05 03:31:15 +0300

    vose predictor

--- src/server/helpers/vose-predictor.js
+++ src/server/helpers/vose-predictor.js
@@ -1,34 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var vose_1 = require("./vose");
-var weights = [40, 10, 30];
-var v = new vose_1.Vose(weights);
 var VosePredictor = (function () {
     function VosePredictor(odds) {
-        this.awayWinVose = null;
+        this.underdogVose = null;
         this.drawVose = null;
-        this.homeWinVose = null;
+        this.favoriteVose = null;
         var homeWin = odds.homeWin, awayWin = odds.awayWin, draw = odds.draw;
         this.homeWinOdds = homeWin;
         this.awayWinOdds = awayWin;
         this.drawOdds = draw;
     }
     VosePredictor.prototype.predict = function () {
+        var underdogWeight;
+        var favoriteWeight;
         var homeWinWeight = Math.round((1 / this.homeWinOdds) * 100);
         var drawWeight = Math.round((1 / this.drawOdds) * 100);
         var awayWinWeight = Math.round((1 / this.awayWinOdds) * 100);
-        var weights = [homeWinWeight, drawWeight, awayWinWeight];
-        var v = new vose_1.Vose(weights);
+        var v = new vose_1.Vose([homeWinWeight, drawWeight, awayWinWeight]);
         var outcomes = ['HOME', 'DRAW', 'AWAY'];
         var outcome = outcomes[v.next()];
         var score = '';
         if (outcome === 'HOME') {
             console.log(homeWinWeight);
-            score = this.getHomeWinScore();
+            if (homeWinWeight > awayWinWeight) {
+                score = this.getFavouriteScore(true);
+            }
+            else {
+                score = this.getUnderdogScore(true);
+            }
         }
         if (outcome === 'AWAY') {
             console.log(awayWinWeight);
-            score = this.getAwayWinScore();
+            if (awayWinWeight > homeWinWeight) {
+                score = this.getFavouriteScore(false);
+            }
+            else {
+                score = this.getUnderdogScore(false);
+            }
         }
         if (outcome === 'DRAW') {
             console.log(drawWeight);
@@ -36,21 +45,33 @@ var VosePredictor = (function () {
         }
         console.log(score);
     };
-    VosePredictor.prototype.getHomeWinScore = function () {
-        var scores = ['1-0', '2-0', '3-0', '4-0', '2-1', '3-1', '4-1', '3-2', '4-2'];
+    VosePredictor.prototype.getFavouriteScore = function (isHomeTeam) {
+        var scores = [];
+        if (isHomeTeam) {
+            scores = ['1-0', '2-0', '3-0', '4-0', '2-1', '3-1', '4-1', '3-2', '4-2'];
+        }
+        else {
+            scores = ['0-1', '0-2', '0-3', '0-4', '1-2', '1-3', '1-4', '2-3', '2-4'];
+        }
         var weights = [98, 81, 48, 23, 89, 52, 25, 28, 14];
-        if (this.homeWinVose == null) {
-            this.homeWinVose = new vose_1.Vose(weights);
+        if (this.favoriteVose == null) {
+            this.favoriteVose = new vose_1.Vose(weights);
         }
-        return scores[this.homeWinVose.next()];
+        return scores[this.favoriteVose.next()];
     };
-    VosePredictor.prototype.getAwayWinScore = function () {
-        var scores = ['0-1', '0-2', '0-3', '1-2', '1-3', '2-3'];
-        var weights = [63, 34, 14, 56, 23, 18];
-        if (this.awayWinVose == null) {
-            this.awayWinVose = new vose_1.Vose(weights);
+    VosePredictor.prototype.getUnderdogScore = function (isHomeTeam) {
+        var scores = [];
+        if (isHomeTeam) {
+            scores = ['1-0', '2-0', '2-1', '3-1', '3-2'];
         }
-        return scores[this.awayWinVose.next()];
+        else {
+            scores = ['0-1', '0-2', '1-2', '1-3', '2-3'];
+        }
+        var weights = [63, 34, 56, 23, 18];
+        if (this.underdogVose == null) {
+            this.underdogVose = new vose_1.Vose(weights);
+        }
+        return scores[this.underdogVose.next()];
     };
     VosePredictor.prototype.getDrawScore = function () {
         var scores = ['0-0', '1-1', '2-2', '3-3'];
@@ -58,13 +79,16 @@ var VosePredictor = (function () {
         if (this.drawVose == null) {
             this.drawVose = new vose_1.Vose(weights);
         }
-        return scores[v.next()];
+        return scores[this.drawVose.next()];
     };
     return VosePredictor;
 }());
-var predictor = new VosePredictor({ homeWin: 1.61, draw: 3.75, awayWin: 5.5 });
+var v = new VosePredictor({
+    homeWin: 1.61,
+    draw: 3.75,
+    awayWin: 5.5
+});
 for (var i = 0; i < 12; i++) {
-    console.log(i);
-    predictor.predict();
+    v.predict();
 }
 //# sourceMappingURL=vose-predictor.js.map
--- src/server/helpers/vose-predictor.ts
+++ src/server/helpers/vose-predictor.ts
@@ -1,6 +1,4 @@
 import {Vose} from './vose';
-var weights = [40, 10, 30]
-var v = new Vose(weights);
 
 class VosePredictor{
   constructor(odds: any) {
@@ -13,50 +11,66 @@ class VosePredictor{
   homeWinOdds: number;
   drawOdds: number;
   awayWinOdds: number;
-  awayWinVose: any = null;
+  underdogVose: any = null;
   drawVose: any = null;
-  homeWinVose: any = null;
+  favoriteVose: any = null;
 
   predict() {
+    let underdogWeight: number;
+    let favoriteWeight: number;
     let homeWinWeight = Math.round((1 / this.homeWinOdds) * 100);
     let drawWeight = Math.round((1 / this.drawOdds) * 100);
     let awayWinWeight = Math.round((1 / this.awayWinOdds) * 100);
-    let weights = [homeWinWeight, drawWeight, awayWinWeight];
-    let v = new Vose(weights);
+    let v = new Vose([homeWinWeight, drawWeight, awayWinWeight]);
     let outcomes: string[] = ['HOME','DRAW','AWAY']
     let outcome = outcomes[v.next()]
     let score = '';
     if (outcome === 'HOME') {
-      console.log(homeWinWeight)
-      score = this.getHomeWinScore();
+      if(homeWinWeight > awayWinWeight) {
+        score = this.getFavouriteScore(true);
+      } else {
+        score = this.getUnderdogScore(true);
+      }
     }
     if (outcome === 'AWAY') {
-      console.log(awayWinWeight)
-      score = this.getAwayWinScore();
+      if(awayWinWeight > homeWinWeight) {
+        score = this.getFavouriteScore(false);
+      } else {
+        score = this.getUnderdogScore(false);
+      }
     }
     if (outcome === 'DRAW') {
-      console.log(drawWeight)
       score = this.getDrawScore();
     }
-    console.log(score);
   } 
 
-  getHomeWinScore() {
-    var scores = ['1-0', '2-0', '3-0', '4-0', '2-1', '3-1', '4-1', '3-2', '4-2'] 
-    var weights = [  98,    81,    48,    23,    89,    52,    25,    28,   14]
-    if(this.homeWinVose == null) {
-      this.homeWinVose = new Vose(weights);
+  getFavouriteScore(isHomeTeam: boolean) {
+    let scores: string[] = [];
+    if(isHomeTeam) {
+      scores = ['1-0', '2-0', '3-0', '4-0', '2-1', '3-1', '4-1', '3-2', '4-2'] 
+    } else {
+      scores = ['0-1', '0-2', '0-3', '0-4', '1-2', '1-3', '1-4', '2-3', '2-4'] 
     }
-    return scores[this.homeWinVose.next()]
+    let weights = [98,    81,    48,    23,    89,    52,    25,    28,   14]
+
+    if(this.favoriteVose == null) {
+      this.favoriteVose = new Vose(weights);
+    }
+    return scores[this.favoriteVose.next()]
   }
   
-  getAwayWinScore() {
-    var scores = ['0-1', '0-2', '0-3', '1-2', '1-3', '2-3'] 
-    var weights = [  63,    34,    14,    56,    23,   18]
-    if(this.awayWinVose == null) {
-      this.awayWinVose = new Vose(weights);
+  getUnderdogScore(isHomeTeam: boolean) {
+    let scores: string[] = [];
+    if(isHomeTeam) {
+      scores = ['1-0', '2-0', '2-1', '3-1', '3-2'] 
+    } else {
+      scores = ['0-1', '0-2', '1-2', '1-3', '2-3'] 
     }
-    return scores[this.awayWinVose.next()]
+    let weights = [63,    34,    56,    23,   18]
+    if(this.underdogVose == null) {
+      this.underdogVose = new Vose(weights);
+    }
+    return scores[this.underdogVose.next()]
   }
 
   getDrawScore() {
@@ -65,6 +79,6 @@ class VosePredictor{
     if(this.drawVose == null) {
       this.drawVose = new Vose(weights);
     }
-    return scores[v.next()]
+    return scores[this.drawVose.next()]
   }
 }  
