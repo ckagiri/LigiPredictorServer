3966ee26f67f3f379a5f6bc4977486efcf6f4dc6
ckagiri <charleskagiri@gmail.com>
2017-10-20 08:59:40 +0300

    minor

--- src/server/helpers/goaldiff-calculator.ts
+++ src/server/helpers/goaldiff-calculator.ts
@@ -6,10 +6,7 @@ class GoalDiffCalculator {
     let homeGoalsGd = null;
     let awayGoalsGd = null;
     let bonusEqualGd = 0;
-    let minGd = Math.min(choiceGd, resultGd);
-    if (minGd === 0) {
-      minGd = 1;
-    }
+    let minGd = Math.min(choiceGd, resultGd) || 1;
 
     let choiceOutcome = calcOutcome(choice.goalsHomeTeam, choice.goalsAwayTeam);
     let resultOutcome = calcOutcome(result.goalsHomeTeam, result.goalsAwayTeam);
