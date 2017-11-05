10190014731443e1891ea3c0e8850fcb30169bf8
ckagiri <charleskagiri@gmail.com>
2017-11-05 09:48:43 +0300

    rules

--- src/client/app/rules/rules.controller.js
+++ src/client/app/rules/rules.controller.js
@@ -16,25 +16,25 @@ var app;
                 this.hasJoker = false;
                 this.points = {
                     rules: {
-                        RULE_1: { hasValue: false, value: 0 },
-                        RULE_2: { hasValue: false, value: 0 },
-                        RULE_3: { hasValue: false, value: 0 },
-                        RULE_4: { hasValue: false, value: 0 },
-                        RULE_5: { hasValue: false, value: 0 },
-                        RULE_6: { hasValue: false, value: 0 }
+                        RULE_1: { id: '1', show: false, value: 0, name: 'Correct Match Outcome' },
+                        RULE_2: { id: '2', show: false, value: 0, name: 'Correct Goal Difference' },
+                        RULE_3: { id: '3', show: false, value: 0, name: 'Correct Team Score' },
+                        RULE_4: { id: '4', show: false, value: 0, name: 'Exact Match Score' },
+                        RULE_5: { id: '5', show: false, value: 0, name: 'Team Score of 2 or more' },
+                        RULE_6: { id: '6', show: false, value: 0, name: 'Within 1 goal of Match Score' }
                     },
                     value: 0
                 };
                 this.goalDiff = {
                     rules: {
-                        RULE_1: { hasValue: false, value: 0 },
-                        RULE_2: { hasValue: false, value: 0 },
-                        RULE_3_1: { hasValue: false, value: 0 },
-                        RULE_3_2: { hasValue: false, value: 0 },
-                        RULE_4: { hasValue: false, value: 0 }
+                        RULE_1: { id: '1', show: false, value: 0, name: 'Correct Team Score (Gain)' },
+                        RULE_2: { id: '2', show: false, value: 0, name: 'Incorrect Team Score (Loss)' },
+                        RULE_3_1: { id: '3.1', show: false, value: 0, name: 'Team Score Within 1 (Insurance)' },
+                        RULE_3_2: { id: '3.2', show: false, value: 0, name: 'Correct Goal Difference (Extra)' },
+                        RULE_4: { id: '4', show: false, value: 0, name: 'Least of the Goal-Differences (Gain)' }
                     },
                     value: 0
-                };
+                }; //	RULE_3: {id: 'c', show: false, value: 0, name: 'Correct Outcome, Incorrect Score, And'},
                 this.totalPoints = 0;
                 this.totalGoalDiff = 0;
                 this.activate();
@@ -43,6 +43,16 @@ var app;
                 this.defaultPoints = angular.copy(this.points);
                 this.defaultGoalDiff = angular.copy(this.goalDiff);
                 this.goalsChanged();
+            };
+            RulesController.prototype.jokerChanged = function () {
+                this.totalPoints = 0;
+                this.totalGoalDiff = 0;
+                this.totalPoints += this.points.value;
+                this.totalGoalDiff += this.goalDiff.value;
+                if (this.hasJoker && this.goalDiff.value >= 0) {
+                    this.totalPoints += this.points.value;
+                    this.totalGoalDiff += this.goalDiff.value;
+                }
             };
             RulesController.prototype.goalsChanged = function () {
                 this.totalPoints = 0;
@@ -55,53 +65,48 @@ var app;
                 var resultGd = this.homeScoreResult - this.awayScoreResult;
                 this.processPoints(predictionOutcome, resultOutcome, predictionGd, resultGd);
                 this.processGoalDiff(predictionOutcome, resultOutcome, predictionGd, resultGd);
-                this.totalPoints += this.points.value;
-                this.totalGoalDiff += this.goalDiff.value;
-                if (this.hasJoker && this.goalDiff.value >= 0) {
-                    this.totalPoints += this.points.value;
-                    this.totalGoalDiff += this.goalDiff.value;
-                }
+                this.jokerChanged();
             };
             RulesController.prototype.processPoints = function (predictionOutcome, resultOutcome, predictionGd, resultGd) {
                 var rules = this.points.rules;
                 if (predictionOutcome === resultOutcome) {
-                    rules.RULE_1.hasValue = true;
+                    rules.RULE_1.show = true;
                     rules.RULE_1.value = 4;
                 }
                 if (predictionGd === resultGd) {
-                    rules.RULE_2.hasValue = true;
+                    rules.RULE_2.show = true;
                     rules.RULE_2.value = 1;
                 }
                 if (this.homeScorePrediction === this.homeScoreResult) {
-                    rules.RULE_3.hasValue = true;
+                    rules.RULE_3.show = true;
                     rules.RULE_3.value += 1;
                 }
                 if (this.awayScorePrediction === this.awayScoreResult) {
-                    rules.RULE_3.hasValue = true;
+                    rules.RULE_3.show = true;
                     rules.RULE_3.value += 1;
                 }
                 if (this.homeScorePrediction === this.homeScoreResult &&
                     this.awayScorePrediction === this.awayScoreResult) {
-                    rules.RULE_4.hasValue = true;
+                    rules.RULE_4.show = true;
                     rules.RULE_4.value = 1;
                 }
                 if (this.homeScorePrediction > 1 && this.homeScoreResult > 1) {
-                    rules.RULE_5.hasValue = true;
+                    rules.RULE_5.show = true;
                     rules.RULE_5.value += 1;
                 }
                 if (this.awayScorePrediction > 1 && this.awayScoreResult > 1) {
-                    rules.RULE_5.hasValue = true;
+                    rules.RULE_5.show = true;
                     rules.RULE_5.value += 1;
                 }
                 if (Math.abs(predictionGd - resultGd) === 1 &&
                     (this.homeScorePrediction === this.homeScoreResult ||
                         this.awayScorePrediction === this.awayScoreResult)) {
-                    rules.RULE_6.hasValue = true;
+                    rules.RULE_6.show = true;
                     rules.RULE_6.value += 1;
                 }
                 for (var key in rules) {
                     var rule = rules[key];
-                    if (rule.hasValue) {
+                    if (rule.show) {
                         this.points.value += rule.value;
                     }
                 }
@@ -113,28 +118,28 @@ var app;
                 var awayGoalsGd = null;
                 var minGd = Math.abs(Math.min(predictionGd, resultGd)) || 1;
                 var rules = this.goalDiff.rules;
-                rules.RULE_4.hasValue = true;
+                rules.RULE_4.show = true;
                 rules.RULE_4.value = minGd;
                 if (predictionOutcome === resultOutcome) {
                     homeGoalsGd = Math.abs(this.homeScorePrediction - this.homeScoreResult);
                     awayGoalsGd = Math.abs(this.awayScorePrediction - this.awayScoreResult);
                     if (homeGoalsGd > 0 && awayGoalsGd > 0 && predictionGd === resultGd) {
-                        rules.RULE_3_2.hasValue = true;
+                        rules.RULE_3_2.show = true;
                         rules.RULE_3_2.value = 1;
                     }
                     if (homeGoalsGd === 1) {
-                        rules.RULE_2.hasValue = false;
+                        rules.RULE_2.show = false;
                         rules.RULE_2.value -= 1;
-                        rules.RULE_3_1.hasValue = true;
+                        rules.RULE_3_1.show = true;
                         rules.RULE_3_1.value += 1;
                     }
                     else {
                         homeGoalsGd = null;
                     }
                     if (awayGoalsGd === 1) {
-                        rules.RULE_2.hasValue = false;
+                        rules.RULE_2.show = false;
                         rules.RULE_2.value -= 1;
-                        rules.RULE_3_1.hasValue = true;
+                        rules.RULE_3_1.show = true;
                         rules.RULE_3_1.value += 1;
                     }
                     else {
@@ -144,22 +149,22 @@ var app;
                 if (homeGoalsGd === null) {
                     homeGoalsGd = Math.abs(this.homeScorePrediction - this.homeScoreResult);
                     if (homeGoalsGd === 0) {
-                        rules.RULE_1.hasValue = true;
+                        rules.RULE_1.show = true;
                         rules.RULE_1.value += this.homeScoreResult || 1;
                     }
                     else {
-                        rules.RULE_2.hasValue = true;
+                        rules.RULE_2.show = true;
                         rules.RULE_2.value -= homeGoalsGd;
                     }
                 }
                 if (awayGoalsGd === null) {
                     awayGoalsGd = Math.abs(this.awayScorePrediction - this.awayScoreResult);
                     if (awayGoalsGd === 0) {
-                        rules.RULE_1.hasValue = true;
+                        rules.RULE_1.show = true;
                         rules.RULE_1.value += this.awayScoreResult || 1;
                     }
                     else {
-                        rules.RULE_2.hasValue = true;
+                        rules.RULE_2.show = true;
                         rules.RULE_2.value -= awayGoalsGd;
                     }
                 }
@@ -167,6 +172,8 @@ var app;
                     var rule = rules[key];
                     this.goalDiff.value += rule.value;
                 }
+                rules.RULE_2.value += rules.RULE_3_1.value;
+                rules.RULE_3_1.value = null;
             };
             RulesController.prototype.calcOutcome = function (home, away) {
                 if (home > away) {
--- src/client/app/rules/rules.controller.ts
+++ src/client/app/rules/rules.controller.ts
@@ -19,26 +19,26 @@ namespace app.rules {
 		hasJoker: boolean = false;		
 		points: any = {
 			rules:  {
-				RULE_1: {hasValue: false, value: 0},
-				RULE_2: {hasValue: false, value: 0},
-				RULE_3: {hasValue: false, value: 0},
-				RULE_4: {hasValue: false, value: 0},
-				RULE_5: {hasValue: false, value: 0},
-				RULE_6: {hasValue: false, value: 0}
+				RULE_1: {id: '1', show: false, value: 0, name: 'Correct Match Outcome'},
+				RULE_2: {id: '2', show: false, value: 0, name: 'Correct Goal Difference'},
+				RULE_3: {id: '3', show: false, value: 0, name: 'Correct Team Score'},
+				RULE_4: {id: '4', show: false, value: 0, name: 'Exact Match Score'},
+				RULE_5: {id: '5', show: false, value: 0, name: 'Team Score of 2 or more'},
+				RULE_6: {id: '6', show: false, value: 0, name: 'Within 1 goal of Match Score'}
 			},
 			value: 0
 		};
 		defaultPoints: any;
 		goalDiff: any = {
 			rules: {
-				RULE_1: {hasValue: false, value: 0},
-				RULE_2: {hasValue: false, value: 0},
-				RULE_3_1: {hasValue: false, value: 0},
-				RULE_3_2: {hasValue: false, value: 0},
-				RULE_4: {hasValue: false, value: 0}
+				RULE_1: {id: '1', show: false, value: 0, name: 'Correct Team Score (Gain)'},
+				RULE_2: {id: '2', show: false, value: 0, name: 'Incorrect Team Score (Loss)'},
+				RULE_3_1: {id: '3.1', show: false, value: 0, name: 'Team Score Within 1 (Insurance)'},
+				RULE_3_2: {id: '3.2', show: false, value: 0, name: 'Correct Goal Difference (Extra)'},
+				RULE_4: {id: '4', show: false, value: 0, name: 'Least of the Goal-Differences (Gain)'}
 			},
 			value: 0
-		};
+		}; //	RULE_3: {id: 'c', show: false, value: 0, name: 'Correct Outcome, Incorrect Score, And'},
 		defaultGoalDiff: any;
 		totalPoints: number = 0;
 		totalGoalDiff: number = 0;
@@ -47,6 +47,17 @@ namespace app.rules {
 			this.defaultPoints = angular.copy(this.points);
 			this.defaultGoalDiff = angular.copy(this.goalDiff);
 			this.goalsChanged();
+		}
+
+		jokerChanged() {
+			this.totalPoints = 0;
+			this.totalGoalDiff = 0;
+			this.totalPoints += this.points.value;
+			this.totalGoalDiff += this.goalDiff.value;
+			if(this.hasJoker && this.goalDiff.value >= 0) {
+				this.totalPoints += this.points.value;
+				this.totalGoalDiff += this.goalDiff.value;
+			}
 		}
 
 		goalsChanged() {
@@ -64,12 +75,7 @@ namespace app.rules {
 			this.processPoints(predictionOutcome, resultOutcome, predictionGd, resultGd);
 			this.processGoalDiff(predictionOutcome, resultOutcome, predictionGd, resultGd);
 
-			this.totalPoints += this.points.value;
-			this.totalGoalDiff += this.goalDiff.value;
-			if(this.hasJoker && this.goalDiff.value >= 0) {
-				this.totalPoints += this.points.value;
-				this.totalGoalDiff += this.goalDiff.value;
-			}
+			this.jokerChanged();
 		}
 
 		processPoints(predictionOutcome: string, resultOutcome: string, 
@@ -77,51 +83,51 @@ namespace app.rules {
 			let rules = this.points.rules;
 
 			if(predictionOutcome === resultOutcome) {
-				rules.RULE_1.hasValue = true;
+				rules.RULE_1.show = true;
 				rules.RULE_1.value = 4;    	
 			}
 
 			if(predictionGd === resultGd) {
-				rules.RULE_2.hasValue = true;
+				rules.RULE_2.show = true;
 				rules.RULE_2.value = 1;			
 			}
 
 			if(this.homeScorePrediction === this.homeScoreResult) {
-      	rules.RULE_3.hasValue = true;
+      	rules.RULE_3.show = true;
 				rules.RULE_3.value += 1;
    	 }
 
     	if(this.awayScorePrediction === this.awayScoreResult) {
-				rules.RULE_3.hasValue = true;
+				rules.RULE_3.show = true;
 				rules.RULE_3.value += 1;    
 			}
 
 			if(this.homeScorePrediction === this.homeScoreResult && 
 				this.awayScorePrediction === this.awayScoreResult) {
-				rules.RULE_4.hasValue = true;
+				rules.RULE_4.show = true;
 				rules.RULE_4.value = 1;    
 			} 
 
 			if(this.homeScorePrediction > 1 && this.homeScoreResult > 1) {
-				rules.RULE_5.hasValue = true;
+				rules.RULE_5.show = true;
 				rules.RULE_5.value += 1;  
 			}
 
 			if(this.awayScorePrediction > 1 && this.awayScoreResult > 1) {
-				rules.RULE_5.hasValue = true;
+				rules.RULE_5.show = true;
 				rules.RULE_5.value += 1; 
 			}
 
 			if(Math.abs(predictionGd - resultGd) === 1 && 
 				(this.homeScorePrediction === this.homeScoreResult || 
 					this.awayScorePrediction === this.awayScoreResult)) {
-				rules.RULE_6.hasValue = true;
+				rules.RULE_6.show = true;
 				rules.RULE_6.value += 1; 
 			}
 
 			for(let key in rules) {
 				let rule = rules[key];
-				if(rule.hasValue) {
+				if(rule.show) {
 					this.points.value += rule.value;
 				}
 			}
@@ -136,31 +142,31 @@ namespace app.rules {
 			let minGd = Math.abs(Math.min(predictionGd, resultGd)) || 1;
 			let rules = this.goalDiff.rules;
 
-			rules.RULE_4.hasValue = true;
+			rules.RULE_4.show = true;
 			rules.RULE_4.value = minGd;
 
 	   	if(predictionOutcome === resultOutcome) {
 				homeGoalsGd = Math.abs(this.homeScorePrediction - this.homeScoreResult);
 				awayGoalsGd = Math.abs(this.awayScorePrediction - this.awayScoreResult);
 				if(homeGoalsGd > 0 && awayGoalsGd > 0 &&  predictionGd === resultGd) {
-					rules.RULE_3_2.hasValue = true;
+					rules.RULE_3_2.show = true;
 					rules.RULE_3_2.value = 1;
 				}    
 				if(homeGoalsGd === 1) {
-					rules.RULE_2.hasValue = false;
+					rules.RULE_2.show = false;
 					rules.RULE_2.value -= 1;
 
-					rules.RULE_3_1.hasValue = true;
+					rules.RULE_3_1.show = true;
 					rules.RULE_3_1.value += 1;
 				} else {
 					homeGoalsGd = null;
 				}
 
 				if(awayGoalsGd === 1) {
-					rules.RULE_2.hasValue = false;
+					rules.RULE_2.show = false;
 					rules.RULE_2.value -= 1;
 
-					rules.RULE_3_1.hasValue = true;
+					rules.RULE_3_1.show = true;
 					rules.RULE_3_1.value += 1;
 				} else {
 					awayGoalsGd = null;
@@ -170,10 +176,10 @@ namespace app.rules {
 			if(homeGoalsGd === null) {
 				homeGoalsGd = Math.abs(this.homeScorePrediction - this.homeScoreResult);
 				if(homeGoalsGd === 0) {
-					rules.RULE_1.hasValue = true;
+					rules.RULE_1.show = true;
 					rules.RULE_1.value += this.homeScoreResult || 1;
 				} else {
-					rules.RULE_2.hasValue = true;
+					rules.RULE_2.show = true;
 					rules.RULE_2.value -= homeGoalsGd;
 				}
 			}
@@ -181,10 +187,10 @@ namespace app.rules {
 			if(awayGoalsGd === null) {
 				awayGoalsGd = Math.abs(this.awayScorePrediction - this.awayScoreResult);
 				if(awayGoalsGd === 0) {
-					rules.RULE_1.hasValue = true;
+					rules.RULE_1.show = true;
 					rules.RULE_1.value += this.awayScoreResult || 1;
 				} else {
-					rules.RULE_2.hasValue = true;
+					rules.RULE_2.show = true;
 					rules.RULE_2.value -= awayGoalsGd;
 				}
 			}
@@ -193,6 +199,8 @@ namespace app.rules {
 					let rule = rules[key];
 					this.goalDiff.value += rule.value;
 			}
+			rules.RULE_2.value += rules.RULE_3_1.value;
+			rules.RULE_3_1.value = null;
 		}
 
 		private calcOutcome(home: number, away: number): string {
